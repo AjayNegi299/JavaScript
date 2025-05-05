474a3a019f8091a354eb6a3fccad061878f93b9a
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-05-05 20:32:23 +0530

    update

---
+++ Number&Maths/index.js
@@ -0,0 +1,26 @@
+const score = 400
+console.log(score)
+
+//new keyword
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+
+//------Maths-------
+
+console.log(Math)
+console.log(Math.round(4.4))
+console.log(Math.max(4,5,6,45,77,5,1,4))
+console.log(Math.floor(Math.random()*10))
+console.log(Math.floor(Math.random()*10))
+console.log(Math.floor(Math.random()*10))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+
+
+
+
