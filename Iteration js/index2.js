e2000c9f1ca7d3670efbe26531736e7be714295f
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-05-11 15:48:40 +0530

    new file added

---
+++ Iteration js/index2.js
@@ -0,0 +1,23 @@
+//---------While Loop-----------
+
+let index = 1
+while (index<=10) {
+    console.log(`value is ${index}`);
+    index = index + 1
+}
+
+//While loop with array
+
+    const myArray = ['akshay', 'akash', 'mayank'];
+    let arr = 0;
+    while (arr < myArray.length) {
+        console.log(myArray[arr]);
+        arr++
+    }
+
+    //do while loop
+    let score = 11
+    do {
+        console.log(`value is ${score}`);
+        score++
+    } while (score<=10);
