c27fe46fdce5285a3ecc39ee0654c5a2aaacafe8
Ajaynegi299 <ajaynegi8700@gmail.com>
2025-05-29 13:08:55 +0530

    update

--- DOM-Navigation/index.js
+++ DOM-Navigation/index.js
@@ -32,8 +32,31 @@ ele.forEach((val)=>{    //we used ele var beacuse ul list same
 /* -----------------NextElementSibling------------------*/
 
 const NextElement = document.getElementById('Vegetables')
-console.log(NextElement);
+// console.log(NextElement);
 const nextSib = NextElement.nextElementSibling
-console.log(nextSib);
-nextSib.style.backgroundColor ='purple'
+// console.log(nextSib);
+// nextSib.style.backgroundColor ='purple'
 
+/* -----------------PreviousElementSibling------------------*/
+
+const elem = document.getElementById('Banana')
+// console.log(elem);
+const preSibling = elem.previousElementSibling
+// console.log(preSibling);
+// preSibling.style.backgroundColor = 'red'
+
+/*------------------ParentElement--------------------------*/
+
+const value = document.getElementById('Patato')
+const parentElement = value.parentElement
+// parentElement.style.backgroundColor = 'purple'
+// parentElement.style.padding = '10px'
+
+/*------------------childern--------------------------*/
+
+const parent = document.getElementById('fruits')
+const child = parent.children
+Array.from(child).forEach(ele =>{
+    console.log(ele);
+    child[0].style.backgroundColor = 'yellow'
+})
